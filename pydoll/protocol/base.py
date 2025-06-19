4a67d6f5b5a80dc8584a9bdd5e1681bfeee08a1f
Tokisaki Kurumi <3201380070@qq.com>
2025-06-19 17:40:15 +0800

    修复了mypy类型检查错误

--- pydoll/protocol/base.py
+++ pydoll/protocol/base.py
@@ -1,30 +1,15 @@
-from typing import Any, Dict, TypedDict, TypeVar
+from typing import Any, Dict, Generic, TypedDict, TypeVar
 
 try:
     from typing import NotRequired
 except ImportError:
     from typing_extensions import NotRequired
 
-T = TypeVar('T')
-
 
 class CommandParams(TypedDict, total=False):
     """Base structure for all command parameters."""
 
     pass
-
-
-class Command(TypedDict):
-    """Base structure for all commands.
-
-    Attributes:
-        method: The command method name
-        params: Optional dictionary of parameters for the command
-    """
-
-    id: NotRequired[int]
-    method: str
-    params: NotRequired[CommandParams]
 
 
 class ResponseResult(TypedDict, total=False):
@@ -43,6 +28,42 @@ class Response(TypedDict):
 
     id: int
     result: ResponseResult
+
+
+# Define Response type variable after Response class is defined
+R = TypeVar('R', bound=Response)
+
+
+class CommandGeneric(TypedDict, Generic[R]):
+    """Base generic structure for all commands with response type parameter.
+
+    Attributes:
+        method: The command method name
+        params: Optional dictionary of parameters for the command
+    """
+
+    id: NotRequired[int]
+    method: str
+    params: NotRequired[CommandParams]
+
+
+# Create non-generic version of Command for backward compatibility
+class Command(TypedDict):
+    """Base structure for all commands.
+
+    Attributes:
+        method: The command method name
+        params: Optional dictionary of parameters for the command
+    """
+
+    id: NotRequired[int]
+    method: str
+    params: NotRequired[CommandParams]
+
+
+# Type alias to support both Command[ResponseType] and Command usage
+# This is needed for backward compatibility
+Command = CommandGeneric  # type: ignore
 
 
 class Event(TypedDict):
