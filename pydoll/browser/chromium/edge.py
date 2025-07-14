111a90a8ffbeb53356c4edddcc104961e46a515d
Tokisaki Kurumi <3201380070@qq.com>
2025-07-14 14:25:28 +0800

    refactor(browser): remove duplicate fingerprint parameters from Edge constructor

--- pydoll/browser/chromium/edge.py
+++ pydoll/browser/chromium/edge.py
@@ -1,9 +1,9 @@
 import platform
-from typing import Optional
+from typing import Optional, cast
 
 from pydoll.browser.chromium.base import Browser
 from pydoll.browser.managers import ChromiumOptionsManager
-from pydoll.browser.options import Options
+from pydoll.browser.options import ChromiumOptions
 from pydoll.exceptions import UnsupportedOS
 from pydoll.utils import validate_browser_paths
 
@@ -13,10 +13,8 @@ class Edge(Browser):
 
     def __init__(
         self,
-        options: Optional[Options] = None,
+        options: Optional[ChromiumOptions] = None,
         connection_port: Optional[int] = None,
-        enable_fingerprint_spoofing: bool = False,
-        fingerprint_config=None,
     ):
         """
         Initialize Edge browser instance.
@@ -24,16 +22,13 @@ class Edge(Browser):
         Args:
             options: Edge configuration options (default if None).
             connection_port: CDP WebSocket port (random if None).
-            enable_fingerprint_spoofing: Whether to enable fingerprint spoofing.
-            fingerprint_config: Configuration for fingerprint generation.
         """
-        options_manager = ChromiumOptionsManager(
-            options,
-            enable_fingerprint_spoofing,
-            fingerprint_config
-        )
+        options_manager = ChromiumOptionsManager(options)
         super().__init__(options_manager, connection_port)
-        self.enable_fingerprint_spoofing = enable_fingerprint_spoofing
+        # Get fingerprint settings from already initialized options
+        # Cast to ChromiumOptions to access fingerprint properties
+        chromium_options = cast(ChromiumOptions, self.options)
+        self.enable_fingerprint_spoofing = chromium_options.enable_fingerprint_spoofing
         self.fingerprint_manager = options_manager.get_fingerprint_manager()
 
     @staticmethod
