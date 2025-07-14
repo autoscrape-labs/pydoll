1f781c13073b36a8e27c0f9b6bc781cc6a70bdeb
Tokisaki Kurumi <3201380070@qq.com>
2025-07-14 14:25:48 +0800

    feat(options): add fingerprint configuration properties to ChromiumOptions

--- pydoll/browser/options.py
+++ pydoll/browser/options.py
@@ -20,6 +20,8 @@ class ChromiumOptions(Options):
         self._arguments = []
         self._binary_location = ''
         self._start_timeout = 10
+        self._enable_fingerprint_spoofing = False
+        self._fingerprint_config = None
 
     @property
     def arguments(self) -> list[str]:
@@ -81,6 +83,46 @@ class ChromiumOptions(Options):
         """
         self._start_timeout = timeout
 
+    @property
+    def enable_fingerprint_spoofing(self) -> bool:
+        """
+        Gets whether fingerprint spoofing is enabled.
+
+        Returns:
+            bool: True if fingerprint spoofing is enabled, False otherwise.
+        """
+        return self._enable_fingerprint_spoofing
+
+    @enable_fingerprint_spoofing.setter
+    def enable_fingerprint_spoofing(self, enabled: bool):
+        """
+        Sets whether fingerprint spoofing is enabled.
+
+        Args:
+            enabled (bool): True to enable fingerprint spoofing, False to disable.
+        """
+        self._enable_fingerprint_spoofing = enabled
+
+    @property
+    def fingerprint_config(self):
+        """
+        Gets the fingerprint configuration.
+
+        Returns:
+            The fingerprint configuration object or None if not set.
+        """
+        return self._fingerprint_config
+
+    @fingerprint_config.setter
+    def fingerprint_config(self, config):
+        """
+        Sets the fingerprint configuration.
+
+        Args:
+            config: The fingerprint configuration object.
+        """
+        self._fingerprint_config = config
+
     def add_argument(self, argument: str):
         """
         Adds a command-line argument to the options.
@@ -95,3 +137,14 @@ class ChromiumOptions(Options):
             self._arguments.append(argument)
         else:
             raise ArgumentAlreadyExistsInOptions(f'Argument already exists: {argument}')
+
+    def enable_fingerprint_spoofing_mode(self, config=None):
+        """
+        Enable fingerprint spoofing with optional configuration.
+
+        Args:
+            config: Optional fingerprint configuration object.
+        """
+        self._enable_fingerprint_spoofing = True
+        if config is not None:
+            self._fingerprint_config = config
